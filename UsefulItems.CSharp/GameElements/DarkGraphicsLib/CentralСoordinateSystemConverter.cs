ec027b5c388bfe64223e19633be062554f074898
DarkLord <dfhrhfan12@yandex.ru>
2021-08-28 20:40:27 +0400

    Add coordinates converter

---
+++ UsefulItems.CSharp/GameElements/DarkGraphicsLib/CentralСoordinateSystemConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UsefulItems.CSharp.GameElements.DarkGraphicsLib.Common;
+
+namespace UsefulItems.CSharp.GameElements.DarkGraphicsLib
+{
+    public class CentralСoordinateSystemConverter
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public CentralСoordinateSystemConverter(int width, int height) => (Width, Height) = (width, height);
+
+        public Vector2 ConvertToScreen(Vector2 vector) => ConvertToScreen(vector, Width, Height);
+
+        public Vector2 ConvertToCentral(Vector2 vector) => ConvertToCentral(vector, Width, Height);
+
+        public static Vector2 ConvertToScreen(Vector2 vector, int width, int height)
+        {
+            var x = width / 2.0 + vector.X;
+            var y = height / 2.0 - vector.Y;
+            return new Vector2(x, y);
+        }
+
+        public static Vector2 ConvertToCentral(Vector2 vector, int width, int height)
+        {
+            var x = vector.X - width / 2.0;
+            var y = height / 2.0 - vector.Y;
+            return new Vector2(x, y);
+        }
+    }
+}
--- UsefulItems.CSharp/Test/CoreConsole/Program.cs
+++ UsefulItems.CSharp/Test/CoreConsole/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using UsefulItems.CSharp.GameElements.DarkGraphicsLib;
+using UsefulItems.CSharp.GameElements.DarkGraphicsLib.Common;
 using UsefulItems.CSharp.Trash.Hacks;
 
 namespace UsefulItems.CSharp.Test.CoreConsole
@@ -9,7 +11,22 @@ namespace UsefulItems.CSharp.Test.CoreConsole
         {
             Console.WriteLine("Hello Core!");
 
-            StringHack.Test();
+            var point = new Vector2(3, 3);
+            Console.WriteLine(point);
+            point = CentralСoordinateSystemConverter.ConvertToScreen(point, 10, 10);
+            Console.WriteLine(point);
+            point = CentralСoordinateSystemConverter.ConvertToCentral(point, 10, 10);
+            Console.WriteLine(point);
+
+            Console.WriteLine();
+
+            point = new Vector2(3, 3);
+            var converter = new CentralСoordinateSystemConverter(10, 10);
+            Console.WriteLine(point);
+            point = converter.ConvertToScreen(point);
+            Console.WriteLine(point);
+            point = converter.ConvertToCentral(point);
+            Console.WriteLine(point);
 
             Console.WriteLine("\nend");
             Console.ReadKey();
