15d27810d78adb3e4e1d90963549731fa1cc8a72
DarkLord <dfhrhfan12@yandex.ru>
2021-08-28 20:26:51 +0400

    Add GameElements folder and Vector2 class

---
+++ UsefulItems.CSharp/GameElements/DarkGraphicsLib/Common/Vector2.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace UsefulItems.CSharp.GameElements.DarkGraphicsLib.Common
+{
+    public class Vector2
+    {
+        #region Base
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public Vector2(double x, double y) => (X, Y) = (x, y);
+
+        public override string ToString() => $"({X}, {Y})";
+
+        public Vector2 Copy() => new Vector2(X, Y);
+
+        public static Vector2 FromAngle(double angle, double length) => new Vector2(length * Math.Cos(angle), length * Math.Sin(angle));
+
+        public static Vector2 Zero => new Vector2(0, 0);
+        #endregion
+
+        #region Properties
+        public double LengthSquare => X * X + Y * Y;
+
+        public double Length => Math.Sqrt(LengthSquare);
+
+        public double Angle => Math.Atan2(Y, X);
+
+        public Vector2 Normalize => new Vector2(X / Length, Y / Length);
+        #endregion
+
+        #region Math
+        public Vector2 Negative() => ((X, Y) = (-X, -Y), this).Item2;
+
+        public Vector2 Add(Vector2 other) => ((X, Y) = (X + other.X, Y + other.Y), this).Item2;
+
+        public Vector2 Add(double other) => ((X, Y) = (X + other, Y + other), this).Item2;
+
+        public Vector2 Sub(Vector2 other) => ((X, Y) = (X - other.X, Y - other.Y), this).Item2;
+
+        public Vector2 Sub(double other) => ((X, Y) = (X - other, Y - other), this).Item2;
+
+        public Vector2 Mult(double other) => ((X, Y) = (X * other, Y * other), this).Item2;
+
+        public Vector2 Div(double other) => ((X, Y) = (X / other, Y / other), this).Item2;
+
+        public double Dot(Vector2 other) => X * other.X + Y * other.Y;
+        #endregion
+
+        public Vector2 Rotate(double angle)
+        {
+            var deltaSin = Math.Sin(angle);
+            var deltaCos = Math.Cos(angle);
+            var old = (X, Y);
+            X = old.X * deltaCos + old.Y * deltaSin;
+            Y = old.Y * deltaCos - old.X * deltaSin;
+            return this;
+        }
+    }
+}
---
+++ UsefulItems.CSharp/Test/CoreWinForms/TestWF.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+using UsefulItems.CSharp.GameElements.DarkGraphicsLib.Common;
+
+namespace UsefulItems.CSharp.Test.CoreWinForms
+{
+    public partial class TestWF : Form
+    {
+        private Vector2 point = new Vector2(20, 20);
+        private Vector2 windowSize;
+        private Graphics graphics;
+
+        public TestWF()
+        {
+            InitializeComponent();
+            windowSize = new Vector2(mainDisplay.Width, mainDisplay.Height);
+            mainDisplay.Image = new Bitmap((int)windowSize.X, (int)windowSize.Y);
+            graphics = Graphics.FromImage(mainDisplay.Image);
+            timer.Start();
+        }
+
+        private void timer_Tick(object sender, System.EventArgs e)
+        {
+            var angle = Math.PI / 50 / 2; // 0.05;
+            point.Rotate(angle);
+            DrawPoint();
+        }
+
+        private void DrawPoint()
+        {
+            //graphics.Clear(Color.Black);
+            DrawPoint(point, Brushes.Red);
+            mainDisplay.Refresh();
+        }
+
+        private void DrawPoint(Vector2 point, Brush brush)
+        {
+            var x = (int)(windowSize.X / 2 + point.X);
+            var y = (int)(windowSize.Y / 2 - point.Y);
+            graphics.FillRectangle(brush, x, y, 1, 1);
+        }
+    }
+}
