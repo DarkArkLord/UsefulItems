d1d70242ed97383ba83eb1a4caeecdb4a0cb7fa9
DarkLord <dfhrhfan12@yandex.ru>
2021-08-29 14:42:37 +0400

    Add test class for particle system

---
+++ UsefulItems.CSharp/GameElements/DarkGraphicsLib/ParticlesSystem2D/Test.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UsefulItems.CSharp.GameElements.DarkGraphicsLib.ParticlesSystem2D.Common;
+
+namespace UsefulItems.CSharp.GameElements.DarkGraphicsLib.ParticlesSystem2D
+{
+    public class Test
+    {
+        private Random random = new Random();
+
+        private List<Particle> particles = new List<Particle>();
+        private List<Emitter> emitters = new List<Emitter>();
+        private List<MassField> fields = new List<MassField>();
+        private const int maxParticles = 20000;
+        private const int emitPerStep = 20;
+
+        public CentralСoordinateSystemConverter Converter { get; set; }
+
+        public Action<Particle> DrawParticle { get; set; }
+        public Action<MassField> DrawField { get; set; }
+        public Action<Emitter> DrawEmitter { get; set; }
+
+        public void AddStartElements()
+        {
+            emitters.Add(EmitterBuilder.NewBuilder
+                .SetRandom(random)
+                .SetPosition(0, 0)
+                .SetSize(1, 1)
+                .SetStartSpeed(0, 2)
+                .SetSpeedDelta(0)
+                .SetAngleDelta(Math.PI)
+                .Create());
+
+            fields.Add(MassFieldBuilder.NewBuilder
+                .SetPosition(-150, 0)
+                .SetMass(900)
+                .Create());
+
+            fields.Add(MassFieldBuilder.NewBuilder
+                .SetPosition(-50, 0)
+                .SetMass(-50)
+                .Create());
+        }
+
+        public void Step()
+        {
+            RemoveParticles();
+            EmitParticles();
+
+            foreach (var particle in particles)
+            {
+                particle.AffectByFields(fields);
+                particle.Move();
+            }
+
+            foreach (var emitter in emitters)
+            {
+                DrawEmitter(emitter);
+            }
+
+            foreach (var field in fields)
+            {
+                DrawField(field);
+            }
+
+            foreach (var particle in particles)
+            {
+                DrawParticle(particle);
+            }
+        }
+
+        private void RemoveParticles()
+        {
+            for (int i = 0; i < particles.Count; i++)
+            {
+                var position = Converter.ConvertToScreen(particles[i].Position);
+                if (position.X < 0 || position.X >= Converter.Width ||
+                    position.Y < 0 || position.Y >= Converter.Height)
+                {
+                    particles.RemoveAt(i);
+                    i--;
+                }
+            }
+        }
+
+        private void EmitParticles()
+        {
+            for (int step = 0; step < emitPerStep; step++)
+            {
+                if (particles.Count >= maxParticles) return;
+                foreach (var emitter in emitters)
+                {
+                    var particle = emitter.EmitParticle();
+                    particles.Add(particle);
+                }
+            }
+        }
+    }
+}
--- UsefulItems.CSharp/Test/CoreWinForms/TestWF.cs
+++ UsefulItems.CSharp/Test/CoreWinForms/TestWF.cs
@@ -11,16 +11,9 @@ namespace UsefulItems.CSharp.Test.CoreWinForms
 {
     public partial class TestWF : Form
     {
-        private Random random = new Random();
-
         private CentralСoordinateSystemConverter converter;
+        private GameElements.DarkGraphicsLib.ParticlesSystem2D.Test testSystem;
         private Graphics graphics;
-
-        private List<Particle> particles = new List<Particle>();
-        private List<Emitter> emitters = new List<Emitter>();
-        private List<MassField> fields = new List<MassField>();
-        private const int maxParticles = 20000;
-        private const int emitPerStep = 20;
 
         public TestWF()
         {
@@ -28,118 +21,47 @@ namespace UsefulItems.CSharp.Test.CoreWinForms
             converter = new CentralСoordinateSystemConverter(mainDisplay.Width, mainDisplay.Height);
             mainDisplay.Image = new Bitmap(mainDisplay.Width, mainDisplay.Height);
             graphics = Graphics.FromImage(mainDisplay.Image);
-            InitParticlesSystem();
+            testSystem = new GameElements.DarkGraphicsLib.ParticlesSystem2D.Test()
+            {
+                Converter = converter,
+                DrawParticle = DrawParticle,
+                DrawEmitter = DrawEmitter,
+                DrawField = DrawField
+            };
+            testSystem.AddStartElements();
             timer.Start();
-        }
-
-        private void InitParticlesSystem()
-        {
-            emitters.Add(EmitterBuilder.NewBuilder
-                .SetRandom(random)
-                .SetPosition(0, 0)
-                .SetSize(1, 1)
-                .SetStartSpeed(0, 2)
-                .SetSpeedDelta(0)
-                .SetAngleDelta(Math.PI)
-                .Create());
-
-            fields.Add(MassFieldBuilder.NewBuilder
-                .SetPosition(-150, 0)
-                .SetMass(900)
-                .Create());
-
-            fields.Add(MassFieldBuilder.NewBuilder
-                .SetPosition(-50, 0)
-                .SetMass(-50)
-                .Create());
         }
 
         private void timer_Tick(object sender, System.EventArgs e)
         {
             timer.Stop();
-            Step();
+            graphics.Clear(Color.Black);
+            testSystem.Step();
+            mainDisplay.Refresh();
             timer.Start();
         }
 
-        private void Step()
-        {
-            RemoveParticles();
-            EmitParticles();
-
-            foreach (var particle in particles)
-            {
-                particle.AffectByFields(fields);
-                particle.Move();
-            }
-
-            graphics.Clear(Color.Black);
-
-            foreach (var emitter in emitters)
-            {
-                DrawEmitter(emitter, Brushes.Yellow);
-            }
-
-            foreach (var field in fields)
-            {
-                var brush = field.Mass == 0
-                    ? Brushes.Gray
-                    : field.Mass < 0
-                        ? Brushes.Red
-                        : Brushes.Green;
-                DrawField(field, brush);
-            }
-
-            foreach (var particle in particles)
-            {
-                DrawParticle(particle.Position, Brushes.Blue);
-            }
-
-            mainDisplay.Refresh();
-        }
-
-        private void RemoveParticles()
-        {
-            for (int i = 0; i < particles.Count; i++)
-            {
-                var position = converter.ConvertToScreen(particles[i].Position);
-                if (position.X < 0 || position.X >= mainDisplay.Width ||
-                    position.Y < 0 || position.Y >= mainDisplay.Height)
-                {
-                    particles.RemoveAt(i);
-                    i--;
-                }
-            }
-        }
-
-        private void EmitParticles()
-        {
-            for (int step = 0; step < emitPerStep; step++)
-            {
-                if (particles.Count >= maxParticles) return;
-                foreach (var emitter in emitters)
-                {
-                    var particle = emitter.EmitParticle();
-                    particles.Add(particle);
-                }
-            }
-        }
-
-        private void DrawEmitter(Emitter emitter, Brush brush)
+        private void DrawEmitter(Emitter emitter)
         {
             var newPosition = converter.ConvertToScreen(emitter.Position);
-            graphics.FillRectangle(brush, (int)newPosition.X, (int)newPosition.Y, (int)emitter.Size.X, (int)emitter.Size.Y);
+            graphics.FillRectangle(Brushes.Yellow, (int)newPosition.X, (int)newPosition.Y, (int)emitter.Size.X, (int)emitter.Size.Y);
         }
 
-        private void DrawField(MassField field, Brush brush)
+        private void DrawField(MassField field)
         {
+            var brush = field.Mass == 0
+                ? Brushes.Gray
+                : field.Mass < 0
+                    ? Brushes.Red
+                    : Brushes.Green;
             var newPosition = converter.ConvertToScreen(field.Position);
             graphics.FillEllipse(brush, (int)newPosition.X - 5, (int)newPosition.Y - 5, 10, 10);
         }
 
-        private void DrawParticle(Vector2 position, Brush brush)
+        private void DrawParticle(Particle particle)
         {
-            var newCoords = converter.ConvertToScreen(position);
-            graphics.FillRectangle(brush, (int)newCoords.X, (int)newCoords.Y, 1, 1);
+            var newCoords = converter.ConvertToScreen(particle.Position);
+            graphics.FillRectangle(Brushes.Blue, (int)newCoords.X, (int)newCoords.Y, 1, 1);
         }
     }
 }
