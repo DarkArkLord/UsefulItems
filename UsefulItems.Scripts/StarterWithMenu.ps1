c8d365545cb696923d2edfedf42076441b11da0e
Aleksei Petrov1 <aleksei_petrov1@epam.com>
2022-04-20 18:34:03 +0400

    Upd starter

--- UsefulItems.Scripts/StarterWithMenu.ps1
+++ UsefulItems.Scripts/StarterWithMenu.ps1
@@ -1,4 +1,6 @@
-﻿# Абсолютный путь к папкам
+﻿param([switch]$kill, [switch]$rebuild)
+
+# Абсолютный путь к папкам
 $MonoPath = 'D:\Program\Sogaz\mono';
 $ImplPath = 'D:\Program\Sogaz\Implementation';
 
@@ -6,13 +8,23 @@ $ImplPath = 'D:\Program\Sogaz\Implementation';
 $Commands = @{
     Move = @{
         Mono = ('cd ' + $MonoPath);
-        Client = ('cd ' + $MonoPath + '\client');
+#        Client = ('cd ' + $MonoPath + '\client');
         Implementation = ('cd ' + $ImplPath);
+        ImplementationConfiguration = ('cd ' + $ImplConfPath + '\configuration');
     };
     Run = @{
         Server = '.\build.ps1 -RunServer';
-        IIS = '.\build.ps1 -RunIS';
-        Client = 'yarn run start';
+#        IIS = '.\build.ps1 -RunIS';
+#        Client = 'yarn run start';
+        Docker = 'docker-compose up --scale server=0';
+    };
+    Kill = @{
+        Docker = 'docker-compose stop';
+    };
+    Build = @{
+        CleanGit = 'git clean -dfx';
+        Install = 'yarn install';
+        Impl = '.\build.ps1 -Build -ExecuteScripts -BuildPrintouts';
     };
 }
 
@@ -28,13 +40,25 @@ $TabsConfig = @{
         Name = 'Server';
         ToExecute = @($Commands.Move.Mono, $Commands.Run.Server)
     };
-    IIS = [TabConfig]@{
-        Name = 'IIS';
-        ToExecute = @($Commands.Move.Mono, $Commands.Run.IIS)
+#    IIS = [TabConfig]@{
+#        Name = 'IIS';
+#        ToExecute = @($Commands.Move.Mono, $Commands.Run.IIS)
+#    };
+#    Client = [TabConfig]@{
+#        Name = 'Client';
+#        ToExecute = @($Commands.Move.Client, $Commands.Run.Client)
+#    };
+    Docker = [TabConfig]@{
+        Name = 'Docker';
+        ToExecute = @($Commands.Move.Implementation, $Commands.Run.Docker)
     };
-    Client = [TabConfig]@{
-        Name = 'Client';
-        ToExecute = @($Commands.Move.Client, $Commands.Run.Client)
+    KillDocker = [TabConfig]@{
+        Name = 'KillDocker';
+        ToExecute = @($Commands.Move.Implementation, $Commands.Kill.Docker)
+    };
+    ReBuildServer = [TabConfig]@{
+        Name = 'Server';
+        ToExecute = @($Commands.Move.ImplementationConfiguration, $Commands.Build.CleanGit, $Commands.Move.Implementation, $Commands.Build.Install, $Commands.Build.Impl, $Commands.Move.Mono, $Commands.Run.Server)
     };
 };
 
@@ -64,10 +88,30 @@ function Create-Tab {
     return $tab;
 }
 
-$Tabs = @{
-	Server = Create-Tab $TabsConfig.Server;
-    IIS = Create-Tab $TabsConfig.IIS;
-    Client = Create-Tab $TabsConfig.Client;
-};
+if($kill)
+{
+    $Tabs = @{
+        Docker = Create-Tab $TabsConfig.KillDocker;
+    };
+}
+else 
+{
+    if($rebuild)
+    {
+        $Tabs = @{
+            Server = Create-Tab $TabsConfig.ReBuildServer;
+        };
+    }
+    else
+    {
+        $Tabs = @{
+            Docker = Create-Tab $TabsConfig.Docker;
+            Server = Create-Tab $TabsConfig.Server;
+        #    IIS = Create-Tab $TabsConfig.IIS;
+        #    Client = Create-Tab $TabsConfig.Client;
+        };
+    }
+}
+
 # TODO:
 # - Сделать, чтобы при закрытии вкладки закрывались запущенные процессы
