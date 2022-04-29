b651ca1e2f3651e62ab8c56ee08cc2c4f611022b
DarkLord <dfhrhfan12@yandex.ru>
2022-04-29 16:25:24 +0400

    Add render v2 and svg

--- UsefulItems.JS/render_lib.js
+++ UsefulItems.JS/render_lib.js
@@ -8,7 +8,7 @@ const HTMLTags = {
     Div: 'div',
 }
 
-function render(item) {
+function render_V1(item) {
     if (item.element) {
         return item.element;
     }
@@ -30,11 +30,78 @@ function render(item) {
     if (item.childs) {
         for (let child of item.childs) {
             if (!child.element) {
-                render(child);
+                render_V1(child);
             }
             element.append(child.element);
         }
     }
 
+    return element;
+}
+
+export function render_V2(tag, attributes, ...childs) {
+    if (tag instanceof Function) {
+        return tag(attributes, ...childs);
+    }
+
+    let element = document.createElement(tag);
+
+    if (attributes) {
+        for (const name in attributes) {
+            let value = attributes[name];
+            element.setAttribute(name, value);
+        }
+    }
+
+    for (const child of childs) {
+        (function addChild(parent, child) {
+            if (Array.isArray(child)) {
+                for (const innerChild of child) {
+                    addChild(parent, innerChild);
+                }
+            } else {
+                parent.appendChild(
+                    child instanceof HTMLElement
+                    ? child
+                    : document.createTextNode(child)
+                );
+            }
+        })(element, child);
+    }
+
+    return element;
+}
+
+const SVGTags = {
+    SVG: 'svg',
+    Group: 'g',
+    Line: 'line',
+    Polyline: 'polyline',
+    Text: 'text'
+}
+
+function renderSVG(item) {
+    if (item.element) {
+        return item.element;
+    }
+    let element = item.element = document.createElementNS('http://www.w3.org/2000/svg', item.tag);
+    if (item.attributes) {
+        for (let name in item.attributes) {
+            let value = item.attributes[name];
+            element.setAttributeNS(null, name, value);
+        }
+    }
+    if (item.value) {
+        element.innerHTML = item.value;
+    }
+    if (item.innerElement) {
+        element.appendChild(item.innerElement);
+    }
+    if (item.childs) {
+        for (let child of item.childs) {
+            renderSVG(child);
+            element.append(child.element);
+        }
+    }
     return element;
 }
