404e4097090d290eeef050409900f1be729b3291
DarkLord <dfhrhfan12@yandex.ru>
2022-07-14 23:05:23 +0400

    Upd powershell script

--- UsefulItems.Scripts/ScriptRunner/test.ps1
+++ UsefulItems.Scripts/ScriptRunner/test.ps1
@@ -1,5 +1,11 @@
 Add-Type -AssemblyName PresentationFramework
-[xml]$xaml = Get-Content -Path .\UsefulItems.Scripts\ScriptRunner\form.xml
+[xml]$xaml = Get-Content -Path .\UsefulItems.Scripts\ScriptRunner\form.xaml
 $reader = (New-Object System.Xml.XmlNodeReader $xaml)
 $window = [Windows.Markup.XamlReader]::Load($reader)
+$xaml.SelectNodes("//*[@*[contains(translate(name(.),'n','N'),'Name')]]") | %{
+    Set-Variable -Name ($_.Name) -Value $window.FindName($_.Name) -Scope Global
+}
+$btnExit.Add_Click({
+    $window.Close()
+})
 $window.ShowDialog()
