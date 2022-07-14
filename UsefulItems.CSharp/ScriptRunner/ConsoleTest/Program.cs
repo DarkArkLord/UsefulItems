64a409c1016204ab0fe349efb57368e688116242
Aleksei Petrov1 <aleksei_petrov1@epam.com>
2022-07-14 17:13:17 +0400

    Add test prj

---
+++ UsefulItems.CSharp/ScriptRunner/ConsoleTest/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic;
+
+namespace ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("START!\n==========");
+
+            using (PowerShell ps = PowerShell.Create())
+            {
+                Print(ps.AddScript("cd C:/")
+                    .Invoke());
+            }
+
+            using (PowerShell ps = PowerShell.Create())
+            {
+                Print(ps.AddScript("dir")
+                    .Invoke());
+            }
+
+            Console.WriteLine("\n==========\nEND!");
+        }
+
+        static void Print(Collection<PSObject> pipeline)
+        {
+            foreach (var item in pipeline)
+            {
+                Console.WriteLine(item.BaseObject.ToString());
+            }
+        }
+    }
+}
